1eaeb49a60fddab76c95a05eccc9f3d7660a5263
thecsnider <snidercloe@gmail.com>
2025-10-13 16:09:33 -0500

    Add files via upload

---
+++ aboutme1.js
@@ -0,0 +1,11 @@
+let name="Cloe Snider"
+let favoritecolor="Pink"
+let favoritehobby="Painting"
+let favoritefood="Grilled Cheese"
+let favoriteanimal="Chihuahua"
+
+console.log(name);
+console.log(favoritecolor);
+console.log(favoritehobby);
+console.log(favoritefood);
+console.log(favoriteanimal);
